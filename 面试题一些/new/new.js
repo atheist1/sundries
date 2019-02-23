78d9a32d2df04594a52c4fcf8e9fe48867bbca5f
atheist1 <1309852694@qq.com>
2019-02-23 16:30:58 +0800

    继承区别

--- 面试题一些/new/new.js
+++ 面试题一些/new/new.js
@@ -1,4 +1,4 @@
-// 实现一个new
+// 实现一个new es5
 
 "use strict";
 var Dog = function(name) {
@@ -55,8 +55,45 @@ console.log(flatten([1, [2],
 // 把对象原型绑定到构造函数的原型
 // 改变对象的this指向
 // 如果构造函数返回的是对象则返回这个对象，不然就返回新创建对象
+// 实际上这种方法在小黄书里叫做oloo object linked to other object
+// 不是利用原型继承，而是利用对象的方式继承
 var __new = function(fn, ...args) {
   var obj = Object.create(fn.prototype)
   const ret = fn.apply(obj, args)
   return ret instanceof Object ? ret : obj
 }
+
+// es6 继承
+// es6的继承就很简单，实现了class 实际上是prototype的语法糖
+class Animal {
+  constructor(name) {
+    this.name = name
+  }
+  sayName() {
+    console.log(this.name)
+  }
+  ping() {
+    console.log(this.name + 'a')
+  }
+}
+class Cat extends Animal {
+  // 这里如果不写constructor会默认生成一个constructor并默认调用super
+  constructor(name) {
+    // super的实际作用是生成了一个父级的this 然后把父级方法添加在子类this上
+    super(name)
+      // super还能这样用
+      // 直接调用父类方法，但是里面的this指向的是子类
+    super.ping()
+  }
+  sayName() {
+    console.log(this.name + 'miao')
+  }
+}
+let cat = new Cat('miaomiao')
+  // es5 es6继承的区别
+  // es5的继承如同上述代码所示，先实现了子类的创建，然后把父类方法
+  // 通过prototype绑定到字类上
+  // es6的继承是es5继承的语法糖，但是实际上也是有区别的
+  // es6的super是先创建了父类的this，然后把为this添加属性
+  // 但是es6的class中子类是没有this的，他实际上是继承了父类的this
+  // 如果你想使用子类的this，在子类构造函数返回一个对象。
