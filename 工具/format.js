a58b190f580b8b970eaaf0a28ecfaa9567cdab1a
atheist1 <1309852694@qq.com>
2019-03-12 09:52:50 +0800

    节流防抖源码

--- 工具/format.js
+++ 工具/format.js
@@ -1,19 +1,18 @@
 // 根据正则生成不同时间格式
-function format(time, pattern) {
-  if (pattern === void 0) { pattern = 'YYYY-MM-dd'; }
+function format(time, pattern = 'YYYY-MM-dd') {
   var date = new Date(time);
   var preFormat = pattern;
   var timeObj = {
-    year: date.getFullYear() + '',
-    month: date.getMonth() + 1 + '',
-    day: date.getDate() + '',
-    hour: date.getHours() + '',
-    minute: date.getMinutes() + '',
-    second: date.getSeconds() + ''
+    year: date.getFullYear(),
+    month: date.getMonth() + 1,
+    day: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+    second: date.getSeconds()
   };
   var arr = [/Y{1,4}/, /M{1,2}/, /d{1,2}/, /H{1,2}/, /m{1,2}/, /s{1,2}/];
   var padding = function(str) {
-    return Number(str) < 10 ? '0' + str : (str + '');
+    return Number(str) < 10 ? '0' + str : str
   };
   arr.forEach(function(item, index) {
     preFormat = preFormat.replace(item, function(all) {
