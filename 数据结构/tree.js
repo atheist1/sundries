2715cd874e3945c17fa4e725eefdf792eb9f3c89
1309852694@qq.com <1309852694@qq.com>
2019-04-18 18:02:07 +0800

    数据结构

---
+++ 数据结构/tree.js
@@ -0,0 +1,158 @@
+// 实现一个树
+// 1.实现一个节点
+class Node{
+  constructor(data) {
+    this.parentNode = null
+    this.leftChild = null
+    this.rightChild = null
+    this.data = data || ''
+  }
+}
+class Tree {
+  constructor(str)  {
+    // A(B(D,E(G)),C(,F))
+    this.node = null
+    this.init(str)
+    // 遇到
+  }
+  // init(str) {
+  //   let stack = new Stack()
+  //   // 左右括号逗号都表示不同的情况
+  //   let newNode = null
+  //   let k = 1
+  //   for (let i = 0 ;  i < str.length; i++) {
+  //     var item = str[i]
+  //     if (item === '(') {
+  //       // 将节点推入
+  //       stack.push(newNode)
+  //       k = 1
+  //     } else if (item === ',') {
+  //       k = 2
+  //     } else if (item === ')'){
+  //       // 将父节点推出
+  //       stack.pop()
+  //     } else { // 单数据出现表示是一个节点
+  //       newNode = new Node(item)
+  //       // 这里是精髓，栈顶元素是当前的父元素，每当碰到左括号时将会把当前父元素推入栈内
+  //       // 遇到右括号证明当前节点父亲处理完毕，出栈
+  //       // 栈顶元素就是当前处理的节点的父元素
+  //       let topNode = stack.top()
+  //       newNode.parentNode = topNode
+  //       if (topNode){
+  //         if (k === 1) { // 表示上一个处理的是左括号
+  //           topNode.leftChild = newNode
+  //         } else if (k === 2) {
+  //           topNode.rightChild = newNode
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+  init(str) {
+    var stack = new Stack
+    let newNode = null
+    var k = 1
+    var topNode = null
+    this.length = 0
+    this.height = 0
+    let walkTree =(str) => {
+      var item = str[0]
+      if (item === '(') {
+        // 将节点推入
+        stack.push(newNode)
+        // 推入节点表示有一个父级
+        this.height ++
+        k = 1
+      } else if (item === ',') {
+        k = 2
+      } else if (item === ')'){
+        this.node = stack.pop()
+      } else { // 单数据出现表示是一个节点
+        item && this.length ++
+        newNode = new Node(item)
+        topNode = stack.top()
+        if (topNode) {
+          newNode.parentNode = topNode
+          if (k === 1) {
+            topNode.leftChild = newNode
+          } else {
+            topNode.rightChild = newNode
+          }
+        }
+      }
+      if (!item) {
+        return
+      }
+      walkTree(str.slice(1))
+    }
+    walkTree(str)
+  }
+  // 返回节点个数
+  size() {
+    let root = this.node
+    let length = 0
+    var countNode = (node) => {
+      if (node) {
+        length ++
+        countNode(node.rightChild)
+        countNode(node.leftChild)
+      }
+    }
+    countNode(root)
+    return length
+  }
+  // 返回数的高度
+  getHeight() {
+    // return this.height
+    // 其他做法，左子树右子树高度取最大值，最后加上当前高度
+  }
+  // 中序优先遍历
+  // 左根右
+  in_order(cb) {
+    let root = this.node
+    let mid = (node) => {
+      if (!node) {  
+        return
+      }
+      mid(node.leftChild)
+      cb.call(this, node.data)
+      mid(node.rightChild)
+    }
+    mid(root)
+  }
+  // 先序优先遍历
+  // 根左右
+  pre_order(cb) {
+    let root = this.node
+    let pre = (node) => {
+      if (!node) {  
+        return
+      }
+      cb.call(this, node.data)
+      pre(node.leftChild)
+      pre(node.rightChild)
+    }
+    pre(root)
+  }
+  // 后序优先遍历
+  // 左右根
+  last_order(cb) {
+    let root = this.node
+    let last = (node) => {
+      if (!node) {  
+        return
+      }
+      last(node.leftChild)
+      last(node.rightChild)
+      cb.call(this, node.data)
+    }
+    last(root)
+  }
+}
+let tree = new Tree('A(B(D,E(G)),C(,F))')
+tree.pre_order(console.log)
+console.log('//////')
+tree.in_order(console.log)
+console.log('//////')
+tree.last_order(console.log)
+console.log(tree.size(),tree.getHeight())
--- 数据结构/栈 stack.js
+++ 数据结构/栈 stack.js
@@ -1,85 +1,85 @@
 // 实现一个栈 
 // 他是一个类,符合先进后出,后进先出的原理
 // 操作有 pop push isEmpty top size clear
-// function Stack () {
-//     var _innerStack = []
-//     let length = 0
-//     this._innerStack = _innerStack
-//     this.length = length
-// }
-// Stack.prototype.push = function (item) {
-//     this._innerStack.push(item)
-//     this.length++
-// }
-// Stack.prototype.pop = function () {
-//     if (this.length <= 0) {
-//         console.error('该栈为空')
-//         return
-//     }
-//     this.length--
-//     return this._innerStack.pop()
-    
-// }
-// Stack.prototype.size = function () {
-//     return this.length || this._innerStack.length
-// }
-// Stack.prototype.top = function () {
-//     return this._innerStack[this.length - 1]
-// }
-// Stack.prototype.clear = function () {
-//     this._innerStack = []
-//     this.length = 0
-// }
-// Stack.prototype.isEmpty = function () {
-//     return this._innerStack === 0 || this.length === 0
-// }
-class Stack {
-    constructor() {
-        var _innerStack = []
-        let length = 0
-        this._innerStack = _innerStack
-        this.length = length
-        this._minStack = new Stack
-    }
-    push(item) {
-        if (this._minStack.isEmpty() || this._minStack.top() > item) {
-            this._minStack.push(item)
-        } else {
-            // 如果后面push的数大于当前栈最小的话,直接推入最小值即可,pop出去不会改变最小值  
-            this._minStack.push(this._minStack.top())
-        }
-        this._innerStack.push(item)
-        this.length++
-    }
-    pop() {
-        if (this.length <= 0) {
-            console.error('该栈为空')
-            return
-        }
-        this.length--
-        this._innerStack.pop()
-        this._minStack.pop()
-        
-    }
-    size() {
-        return this.length || this._innerStack.length
-    }
-    top() {
-        return this._innerStack[this.length - 1]
-    }
-    clear() {
-        this._innerStack = []
-        this.length = 0
-        this._minItem = void 0
-    }
-    isEmpty() {
-        return this._innerStack === 0 || this.length === 0
-    }
-    // 返回栈内最小元素,且时间复杂度为o1
-    min() {
-        return this._minItem
-    }
+function Stack () {
+    var _innerStack = []
+    let length = 0
+    this._innerStack = _innerStack
+    this.length = length
 }
+Stack.prototype.push = function (item) {
+    this._innerStack.push(item)
+    this.length++
+}
+Stack.prototype.pop = function () {
+    if (this.length <= 0) {
+        console.error('该栈为空')
+        return
+    }
+    this.length--
+    return this._innerStack.pop()
+    
+}
+Stack.prototype.size = function () {
+    return this.length || this._innerStack.length
+}
+Stack.prototype.top = function () {
+    return this._innerStack[this.length - 1]
+}
+Stack.prototype.clear = function () {
+    this._innerStack = []
+    this.length = 0
+}
+Stack.prototype.isEmpty = function () {
+    return this._innerStack === 0 || this.length === 0
+}
+// class Stack {
+//     constructor() {
+//         var _innerStack = []
+//         let length = 0
+//         this._innerStack = _innerStack
+//         this.length = length
+//         this._minStack = new Stack
+//     }
+//     push(item) {
+//         if (this._minStack.isEmpty() || this._minStack.top() > item) {
+//             this._minStack.push(item)
+//         } else {
+//             // 如果后面push的数大于当前栈最小的话,直接推入最小值即可,pop出去不会改变最小值  
+//             this._minStack.push(this._minStack.top())
+//         }
+//         this._innerStack.push(item)
+//         this.length++
+//     }
+//     pop() {
+//         if (this.length <= 0) {
+//             console.error('该栈为空')
+//             return
+//         }
+//         this.length--
+//         this._innerStack.pop()
+//         this._minStack.pop()
+        
+//     }
+//     size() {
+//         return this.length || this._innerStack.length
+//     }
+//     top() {
+//         return this._innerStack[this.length - 1]
+//     }
+//     clear() {
+//         this._innerStack = []
+//         this.length = 0
+//         this._minItem = void 0
+//     }
+//     isEmpty() {
+//         return this._innerStack === 0 || this.length === 0
+//     }
+//     // 返回栈内最小元素,且时间复杂度为o1
+//     min() {
+//         return this._minItem
+//     }
+// }
 /**
 let badmintons = new Stack
 badmintons.push('badmin1')
@@ -90,16 +90,16 @@ console.log(badmintons.size(),badmintons.length,badmintons.top(),badmintons.pop(
 */
 /**练习1 */
 // 判断字符串是否有成对括号出现
-var testtify = function (str){
-    var testStack = new Stack
-    for (let i = 0; i < str.length; i ++) {
-        if (str[i] === '(') {
-            testStack.push(true)
-        } else if (str[i] === ')') {
-            testStack.pop()
-        }
-    }
-    return testStack.length === 0
-}
-var str = '()()sd()(sd()fw))('
-console.log(testtify(str))
+// var testtify = function (str){
+//     var testStack = new Stack
+//     for (let i = 0; i < str.length; i ++) {
+//         if (str[i] === '(') {
+//             testStack.push(true)
+//         } else if (str[i] === ')') {
+//             testStack.pop()
+//         }
+//     }
+//     return testStack.length === 0
+// }
+// var str = '()()sd()(sd()fw))('
+// console.log(testtify(str))
