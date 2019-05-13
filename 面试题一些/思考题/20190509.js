f7accf9dfa148eff4b90787094feb5fd04cd6dba
atheist1 <1309852694@qq.com>
2019-05-13 16:50:25 +0800

    解法3

--- 面试题一些/思考题/20190509.js
+++ 面试题一些/思考题/20190509.js
@@ -1,25 +1,5 @@
 // 算法题
 // 一个人的手指从大拇指开始数，到小拇指倒过来数 1 2 3 4 5 6 ,请问第n个数后对应的手指是哪个
-class Stack {
-    constructor() {
-        this._innerStack = [];
-    }
-    push(...items) {
-        this._innerStack.push(...items)
-    }
-    pop(item) {
-        return this._innerStack.pop();
-    }
-    top() {
-        return this._innerStack[this.size() - 1];
-    }
-    size() {
-        return this._innerStack.length || 0;
-    }
-    get(index) {
-        return this._innerStack[index];
-    }
-}
 let getFinger = (num) => {
     /**
         let stack = [1,2,3,4,5,4,3,2]; //[] 1 2 3 4 5 4 3 2 1 2 3 4 5 4 3 2 1 2 3 4 5 4 3 2 1 2
@@ -62,4 +42,57 @@ let getFinger = (num) => {
     }
     return returnVal;
 }
-console.log(getFinger(22))
+console.log(getFinger(22));
+console.log(
+  '2019年05月09日题:从右手大拇指数数，从1开始，至小拇指后颠倒顺序至大拇指，至大拇指后颠倒顺序至小拇指，如下图：'
+);
+
+console.log(`      
+              --
+         --  |3 |
+        |4 | |7 |  --
+    --  |6 | |11| |2 |
+   |5 | |..| |  | |8 |
+   |  | |  | |  | |10|
+   |  | |  | |  | |  |  ---
+   |  | |  | |  | |  | | 1 |
+   |  | |  | |  | |  | | 9 |
+   |  | |  | |  | |  | |   |
+`);
+
+console.log('问：第1314个数在哪个手指?');
+
+function findFinger(times) {
+  const fingers = ['大拇指', '食指', '中指', '无名指', '小指'];
+  let idx = 0;
+  let sequence = true; // 顺序
+  let finger;
+  const len = times % 8 === 0 ? 8 : times % 8;
+  for (let t = 0; t < len; t += 1) {
+    // 标记当前数对应的指头
+    finger = fingers[idx];
+    // console.log('t', t + 1, idx, finger);
+    // 正序
+    if (sequence) {
+      idx += 1;
+      if (idx === 5) {
+        idx = 3;
+        sequence = false;
+      }
+    } else {
+      // 反序
+      idx -= 1;
+      if (idx === 0) {
+        idx = 0;
+        sequence = true;
+      }
+    }
+  }
+  console.log(`第${times}个数对应的手指为：${finger}`);
+}
+
+function main() {
+  findFinger(1314);
+}
+
+main();
