2cd2a0fe1b4440260238df4641ee89c0850b103d
atheist1 <1309852694@qq.com>
2020-05-23 17:56:35 +0800

    leetcode 76最小覆盖子串

---
+++ 面试题一些/leetcode/字符串/76.困难.最小覆盖子串.js
@@ -0,0 +1,14 @@
+/****
+ * 76. 最小覆盖子串
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+输出: "BANC"
+ */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+
+};
