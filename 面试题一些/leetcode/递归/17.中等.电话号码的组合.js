a95a44bac35a473ffcdbaa9bec7d744745368c2f
1309852694@qq.com <1309852694@qq.com>
2020-08-13 11:49:06 +0800

    leetcode10

--- 面试题一些/leetcode/递归/17.中等.电话号码的组合.js
+++ 面试题一些/leetcode/递归/17.中等.电话号码的组合.js
@@ -1,3 +1,30 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: qitianle
+ * @Date: 2019-11-07 17:08:15
+ * @LastEditors: qitianle
+ * @LastEditTime: 2020-08-12 10:52:57
+ */
+/**
+ * 
+ * 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。
+
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+
+
+示例:
+
+输入："23"
+输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+说明:
+尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/letter-combinations-of-a-phone-number
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 /**
  * @param {string} digits
  * @return {string[]}
@@ -67,3 +94,25 @@ var letterCombinations = function (digits) {
   }
   return getResult(digitsArr);
 };
+var letterCombinations = function (digits) {
+  var map = [ '', ['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i'], ['j', 'k', 'l'], ['m', 'n', 'o'], ['p', 'q', 'r', 's'], ['t', 'u', 'v'], ['w', 'x', 'y', 'z']];
+  let arr = digits.split('').map((item) => {
+    return map[item - 1];
+  })
+  let dfs = function (arr) {
+    let temp = [];
+    if (arr.length >= 2) {
+      for (let i = 0; i < arr[0].length; i += 1) {
+        for (let j = 0; j < arr[1].length; j += 1){
+          temp.push(arr[0][i] + arr[1][j]);
+        }
+      }
+      arr.splice(0, 2, temp);
+      dfs(arr);
+    }
+    return arr;
+  }
+  dfs(arr)
+  return arr[0];
+}
+console.log(letterCombinations('223'));
