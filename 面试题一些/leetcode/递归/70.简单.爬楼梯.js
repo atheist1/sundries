20b88af5c84f29f2070259430a5ff3ee0cd9e646
1309852694@qq.com <1309852694@qq.com>
2020-08-15 15:54:35 +0800

    leetcode 70爬楼

--- 面试题一些/leetcode/递归/70.简单.爬楼梯.js
+++ 面试题一些/leetcode/递归/70.简单.爬楼梯.js
@@ -1,8 +1,43 @@
- /**
- * @param {number} n
- * @return {number}
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: qitianle
+ * @Date: 2019-10-16 11:45:07
+ * @LastEditors: qitianle
+ * @LastEditTime: 2020-08-15 15:54:09
  */
-var climbStairs = function(n) {
+/***
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+示例 2：
+
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/climbing-stairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+/**
+* @param {number} n
+* @return {number}
+*/
+var climbStairs = function (n) {
     // 普通算法
     // var fibs = (n, n1, n2) => {
     //     if (n === 1) {
@@ -32,7 +67,7 @@ var climbStairs = function(n) {
     if (n === 1) {
         return 1;
     } else if (n === 2) {
-        return 2;           
+        return 2;
     } else {
         for (let i = 2; i < n; i++) {
             temp = a + b;
